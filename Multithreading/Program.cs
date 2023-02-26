3350e05fe5b0700fe75620d1d4efb25125e107b4
USER <USER@DESKTOP-20DRNS1>
2023-02-27 00:43:23 +0330

    ...

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Multithreading
@@ -10,6 +11,17 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("**********Current Thread Informations***************\n");
+            Thread t = Thread.CurrentThread;
+            t.Name = "Primary_Thread";
+
+            Console.WriteLine("Thread Name: {0}", t.Name);
+            Console.WriteLine("Thread Status: {0}", t.IsAlive);
+            Console.WriteLine("Priority: {0}", t.Priority);
+            Console.WriteLine("Context ID: {0}", Thread.CurrentContext.ContextID);
+            Console.WriteLine("Current application domain: {0}", Thread.GetDomain().FriendlyName);
+
+            Console.ReadKey();
         }
     }
 }
