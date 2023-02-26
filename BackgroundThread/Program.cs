6661d8f3ab07f1270d05b5e54d93314e32163ea3
USER <USER@DESKTOP-20DRNS1>
2023-02-27 01:24:34 +0330

    Background Thread
    The process of the application keeps running as long as at least one foreground thread is running. If more than one foreground thread is running and the Main() method ends, the process of the application keeps active until all foreground threads finish their work.
    
    When you create a thread with the Thread class, you can define it as either a foreground or background thread by setting the property IsBackground. The Main() method set this property of the thread t to false. After setting the new thread, the main thread just writes an end message to the console. The new thread writes a start and an end message and in between it sleeps for 2 seconds.

---
+++ BackgroundThread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(myFun);
+            t.Name = "Thread1";
+            t.IsBackground = true;
+            t.Start();
+            Console.WriteLine("Main thread Running");
+        }
+
+        static void myFun()
+        {
+            Console.WriteLine("Thread {0} started", Thread.CurrentThread.Name);
+            Thread.Sleep(2000);
+            Console.WriteLine("Thread {0} completed", Thread.CurrentThread.Name);
+        }
+    }
+}
---
+++ SimpleThreadCreation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreadCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Thread t1 = new Thread(() => myFun("t1"));
+            Thread t2 = new Thread(() => myFun("t2"));
+            Thread t3 = new Thread(() => myFun("t3"));
+            Thread t4 = new Thread(() => myFun("t4"));
+            Thread t5 = new Thread(() => myFun("t5"));
+            Thread t6 = new Thread(() => myFun("t6"));
+            Thread t7 = new Thread(() => myFun("t7"));
+            Thread t8 = new Thread(() => myFun("t8"));
+            Thread t9 = new Thread(() => myFun("t9"));
+            Thread t10 = new Thread(() => myFun("t10"));
+
+            t1.IsBackground = true;
+            t2.IsBackground = true;
+            t3.IsBackground = true;
+            t4.IsBackground = true;
+            t5.IsBackground = true;
+            t6.IsBackground = true;
+            t7.IsBackground = true;
+            t8.IsBackground = true;
+            t9.IsBackground = true;
+            t10.IsBackground = true;
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+            t6.Start();
+            t7.Start();
+            t8.Start();
+            t9.Start();
+            t10.Start();
+
+            Console.WriteLine("Main thread Running");
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        public static Task myFun(string threadName)
+        {
+           // Task.Delay(1000);
+      Thread.Sleep(1000);
+            Console.WriteLine($"Running other Thread {threadName}");
+            return Task.CompletedTask;
+        }
+    }
+}
